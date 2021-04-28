8cf57f80a45b3c65162cb30be90e46a11051c496
kumarswengineer <kumarswengineer@gmail.com>
2021-04-28 14:23:55 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
+//https://stackblitz.com/edit/angular-10-registration-login-example-dndzag?file=src%2Fapp%2Fusers%2Fusers-routing.module.ts
